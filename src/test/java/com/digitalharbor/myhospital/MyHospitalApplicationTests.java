e0948a7b1200e9c27674b463c10aafa67e5f37e9
Mijael Callejas <mijael.rcf@gmail.com>
2020-12-15 21:52:21 -0400

    Creation project

---
+++ src/test/java/com/digitalharbor/myhospital/MyHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalharbor.myhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
